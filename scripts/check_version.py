31d99bfda6f4b3a439ecaba609644ebedb271817
Sune Debel <sdeb@itu.dk>
2019-09-30 10:22:21 +0200

    chore: setup release job on circleci

---
+++ scripts/check_version.py
@@ -0,0 +1,27 @@
+import re
+import sys
+
+from pfun.io import read_file, IO
+
+
+class MalformedTomlError(Exception):
+    pass
+
+
+def get_version(toml: str) -> str:
+    match = re.search(r'version = \"([0-9]+\.[0-9]+\.[0-9]+)\"', toml)
+    if match is None:
+        raise MalformedTomlError('Could not find version in pyproject.toml')
+    else:
+        return match[1]
+
+
+def check_version(toml: str) -> IO[None]:
+    actual_version = get_version(toml)
+    expected_version = sys.argv[2]
+    assert actual_version == expected_version, f'version "{actual_version}"" in pyproject.toml did not match "{expected_version}""'
+    return IO(None)
+
+
+if __name__ == '__main__':
+    read_file(sys.argv[1]).and_then(check_version).run()
