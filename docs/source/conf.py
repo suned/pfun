817542b0de22a110ed58abba066dceb958ed665c
Sune Debel <sad@hypefactors.com>
2019-10-09 14:07:38 +0200

    fix: try to correct broken api docs on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,20 +1,11 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
-
 project = 'pfun'
 copyright = '2019, Sune Debel'
 author = 'Sune Debel'
 version = ''
-release = '0.1.0'
+release = '0.5.1'
 source_suffix = ['.rst', '.md']
 master_doc = 'index'
-pygments_style = 'sphinx'
-html_theme = 'sphinx_rtd_theme'
-extensions = [
-    'recommonmark',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx'
-]
+extensions = ['recommonmark', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
