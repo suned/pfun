92de564af66417390d0e3b19be659b4e7a1bbc2f
Sune Debel <sune.debel@leoilab.com>
2021-04-02 23:40:57 +0200

    lint

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from setuptools.extension import Extension
 
 
 def build(setup_kwargs):
-    extensions = [Extension("pfun.effect", ['pfun/effect.c'])]
+    extensions = [Extension("pfun.effect", ['src/pfun/effect.c'])]
     setup_kwargs.update(
         {
             "ext_modules": extensions
