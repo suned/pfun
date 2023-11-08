f8076cc114019a99ff0659ec50f2fb7aaaeb4271
Stephen Daves <stephen@stephendaves.com>
2023-11-08 14:18:57 -0500

    Upgrade to python3.11

--- build.py
+++ build.py
@@ -1,10 +1,15 @@
 from setuptools.extension import Extension
+import os
 
 
 def build(setup_kwargs):
+    os.system("cythonize src/pfun/effect.pyx")
     extensions = [Extension("pfun.effect", ['src/pfun/effect.c'])]
     setup_kwargs.update(
         {
             "ext_modules": extensions
         }
     )
+
+if __name__ == '__main__':
+    build({})
