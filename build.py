eb6e553794f7f20d420024f5b33e488cd064f936
Stephen Daves <stephen@stephendaves.com>
2023-11-08 21:53:07 -0500

    Use api for build

--- build.py
+++ build.py
@@ -1,15 +1,14 @@
 from setuptools.extension import Extension
-import os
+from Cython.Build import cythonize
 
 
 def build(setup_kwargs):
-    os.system("cythonize src/pfun/effect.pyx")
-    extensions = [Extension("pfun.effect", ['src/pfun/effect.c'])]
+    cythonize("src/pfun/effect.pyx")
+    extensions = [
+        Extension("pfun.effect", ['src/pfun/effect.c']),
+    ]
     setup_kwargs.update(
         {
             "ext_modules": extensions
         }
     )
-
-if __name__ == '__main__':
-    build({})
