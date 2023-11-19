2fee0ec2d40280e810e996b716e90d1cdd5d9b87
Stephen Daves <stephen@stephendaves.com>
2023-11-19 15:19:19 -0500

    chore: add python 3.11 support (#125)

--- build.py
+++ build.py
@@ -1,8 +1,12 @@
+from Cython.Build import cythonize
 from setuptools.extension import Extension
 
 
 def build(setup_kwargs):
-    extensions = [Extension("pfun.effect", ['src/pfun/effect.c'])]
+    cythonize("src/pfun/effect.pyx")
+    extensions = [
+        Extension("pfun.effect", ['src/pfun/effect.c']),
+    ]
     setup_kwargs.update(
         {
             "ext_modules": extensions
